289b15bef2a54541a9d105d21379087166cee3eb
Tomppahh <Tommi.Paakkunainen@student.lut.fi>
2025-12-17 01:53:29 +0200

    AOC3 Part 1 completed

--- AOC3/dist/AOC3.js
+++ AOC3/dist/AOC3.js
@@ -4,16 +4,28 @@ const fs = require("fs");
 const readFiletoArray = (filePath) => {
     const data = fs.readFileSync(filePath, "utf8");
     const dataArray = data.split("\r\n");
-    return dataArray.map((s) => s.trim());
+    return dataArray.map((s) => s.trim()).filter((s) => s !== "");
+    // remove whitespaces and empty rows
 };
 const filePath = "./data/datafile.txt";
 const fileDataArray = readFiletoArray(filePath);
-console.log(fileDataArray);
-// apufunktio harjotuksen vuoksi
-const convertStringToNumber = (arr) => {
-    let numberArray = arr.map(Number);
-    console.log(numberArray);
-    return numberArray;
+const findLargestNumbersfromRow = (arr) => {
+    let sumOfGlueNumbers = 0;
+    for (let index = 0; index < arr.length; index++) {
+        let value = arr[index];
+        const digits = value.split("").map(Number);
+        let currentMax = 0;
+        // Check all pairs (i, j) where i < j
+        for (let i = 0; i < digits.length; i++) {
+            for (let j = i + 1; j < digits.length; j++) {
+                let gluenumber = Number(String(digits[i]) + String(digits[j]));
+                if (gluenumber > currentMax) {
+                    currentMax = gluenumber;
+                }
+            }
+        }
+        sumOfGlueNumbers += currentMax;
+    }
+    console.log(`Sum of joltages is: ${sumOfGlueNumbers}`);
 };
-const findLargestNumberfromRow = (arr) => { };
-const NumberDataArray = convertStringToNumber(fileDataArray);
+findLargestNumbersfromRow(fileDataArray);
--- AOC3/src/AOC3.ts
+++ AOC3/src/AOC3.ts
@@ -3,24 +3,33 @@ import * as fs from "fs";
 const readFiletoArray = (filePath: string): string[] => {
 	const data = fs.readFileSync(filePath, "utf8");
 	const dataArray = data.split("\r\n");
-	return dataArray.map((s) => s.trim());
+	return dataArray.map((s) => s.trim()).filter((s) => s !== "");
+	// remove whitespaces and empty rows
 };
 
 const filePath = "./data/datafile.txt";
 const fileDataArray = readFiletoArray(filePath);
 
-console.log(fileDataArray);
+const findLargestNumbersfromRow = (arr: string[]) => {
+	let sumOfGlueNumbers = 0;
 
-// helper function to convert string array into a number array
-const convertStringToNumber = (arr: string[]): number[] => {
-	let numberArray: number[] = arr.map(Number);
-	console.log(numberArray);
-	return numberArray;
-};
-// converted data ready to be given to next function
-const NumberDataArray = convertStringToNumber(fileDataArray);
+	for (let index = 0; index < arr.length; index++) {
+		let value = arr[index];
+		const digits = value.split("").map(Number);
+		let currentMax = 0;
 
-const findLargestNumbersfromRow = (arr: number[]) => {
-	let largest = 0;
-	let secondlargest = 9;
+		// Check all pairs (i, j) where i < j
+		for (let i = 0; i < digits.length; i++) {
+			for (let j = i + 1; j < digits.length; j++) {
+				let gluenumber = Number(String(digits[i]) + String(digits[j]));
+				if (gluenumber > currentMax) {
+					currentMax = gluenumber;
+				}
+			}
+		}
+		sumOfGlueNumbers += currentMax;
+	}
+	console.log(`Sum of joltages is: ${sumOfGlueNumbers}`);
 };
+
+findLargestNumbersfromRow(fileDataArray);
