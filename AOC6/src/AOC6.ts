797f28c0ceec8691c5c1909c071693b2621d8255
root <root@ADMINIS-MRLBA96.localdomain>
2025-12-25 15:38:26 +0200

    AOC6 part 1 done

--- AOC6/src/AOC6.ts
+++ AOC6/src/AOC6.ts
@@ -1,1 +1,62 @@
-console.log("asda");
+import * as fs from "fs";
+
+const dataStream = fs.readFileSync("data/datafile.txt", "utf-8");
+const grid: string[][] = dataStream
+	.split("\n")
+	.map((line) => line.trim())
+	.filter((line) => line !== "")
+	.map((line) =>
+		line
+			.split(/[\s,]+/)
+			.map((item) => item.trim())
+			.filter((item) => item !== "")
+	);
+
+const operatorArray = grid.splice(-1, 1)[0];
+
+// Remove separator columns in-place (columns that are empty in all rows)
+for (let col = operatorArray.length - 1; col >= 0; col--) {
+	let isSeparator = true;
+	for (let row = 0; row < grid.length; row++) {
+		if (grid[row][col] && grid[row][col].trim() !== "") {
+			isSeparator = false;
+			break;
+		}
+	}
+	if (isSeparator) {
+		for (let row = 0; row < grid.length; row++) {
+			grid[row].splice(col, 1);
+		}
+		operatorArray.splice(col, 1);
+	}
+}
+
+let sumArray: number[] = [];
+let multiplyArray: number[] = [];
+const gridWith = grid.length > 0 ? grid[0].length : 0;
+
+let totalSum: number = 0;
+for (let column = 0; column < gridWith; column++) {
+	let sum: number = 0;
+	if (operatorArray[column] === "+") {
+		for (let row = 0; row < grid.length; row++) {
+			sum += Number(grid[row][column]);
+		}
+		sumArray.push(sum);
+		// console.log(`column index: ${column}`)
+	} else if (operatorArray[column] === "*") {
+		let multiply = 1;
+		for (let row = 0; row < grid.length; row++) {
+			multiply *= Number(grid[row][column]);
+		}
+		multiplyArray.push(multiply);
+		// console.log(`column index: ${column}`)
+	}
+}
+totalSum = sumArray.reduce((a, b) => a + b, 0);
+
+let totalMultiply = multiplyArray.reduce((a, b) => a + b, 0);
+
+const finalAnswer = totalSum + totalMultiply;
+
+console.log(`finalAnswer: ${finalAnswer}`);
